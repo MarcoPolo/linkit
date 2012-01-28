8630a77e28f8f77583b8a9bbc135d131858446d3
Marco Munizaga <marco.munizaga@grooveshark.com>
2012-01-28 12:21:27 -0500

    updated laptop info

--- app.js
+++ app.js
@@ -5,9 +5,12 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , apiRoute = require('./routes/api.js')
+  , less = require('less')
+  , apiRoute = require('./routes/api.js');
 
 var app = module.exports = express.createServer();
+
+//less
 
 // Configuration
 
@@ -18,6 +21,7 @@ app.configure(function(){
   app.use(express.cookieParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
   app.use(express.static(__dirname + '/public'));
 });
 
